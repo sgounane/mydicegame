905675123e2c37b7e3059beb980a65a98efed87f
gnu <gounane.said@gmail.com>
2021-05-27 15:42:46 +0100

    user account

--- game.py
+++ game.py
@@ -3,17 +3,22 @@ from werkzeug.utils import redirect
 
 app=Flask(__name__)
 
+global user
+user=""
 @app.route("/")
 def index():
     return render_template("index.html")
 
 @app.route("/game")
 def game():
-    return render_template("game.html")
+    global user
+    return render_template("game.html",user=user)
 
 @app.route("/login",methods=["GET","POST"])
 def login():
     if request.method=="POST":
+        global user
+        user=request.form.get("user")
         return redirect('/game')
     return render_template("login.html")
 
